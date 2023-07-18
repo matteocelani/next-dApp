d48ba3a016c3460b08dfb47fd620f975af63d43b
Matteo Celani <matteocelani4@gmail.com>
2023-07-18 15:48:42 +0200

    Wagmi Migration
    Provider → PublicClient

--- src/lib/types/reduxInterface.ts
+++ src/lib/types/reduxInterface.ts
@@ -1,10 +1,10 @@
-import { Provider, Address } from "@wagmi/core";
+import { PublicClient, Address } from "@wagmi/core";
 import { Signer } from "ethers";
 
 export interface initialState {
   address?: Address;
   ensName?: string;
   balance?: number;
-  provider?: Provider;
+  provider?: PublicClient;
   signer?: Signer;
 }
