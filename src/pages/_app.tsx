7d6866f0a2c8378091e4d4dd7fb8c6e73b764791
Matteo Celani <26189482+matteocelani@users.noreply.github.com>
2023-11-15 20:15:32 +0100

    Eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import type { AppProps } from "next/app";
 //Importing Next Themes
 import { ThemeProvider } from "next-themes";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 //Importing Next
 import Head from "next/head";
 //Importing Hooks
