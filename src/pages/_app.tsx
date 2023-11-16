96f943c83a8488714c8b6d9acf898f8401023ed2
Matteo Celani <26189482+matteocelani@users.noreply.github.com>
2023-11-16 20:34:44 +0100

    Tanstack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import type { AppProps } from "next/app";
 //Importing Next Themes
 import { ThemeProvider } from "next-themes";
+//TanStack
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 //Importing Layout
 import Layout from "@/layout";
 //Importing Styles
@@ -109,6 +111,9 @@ const theme = merge(lightTheme(), {
   },
 } as Theme);
 
+// Create a client
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
@@ -119,9 +124,11 @@ export default function App({ Component, pageProps }: AppProps) {
           chains={chains}
           theme={theme}
         >
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          <QueryClientProvider client={queryClient}>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </QueryClientProvider>
         </RainbowKitProvider>
       </WagmiConfig>
     </ThemeProvider>
