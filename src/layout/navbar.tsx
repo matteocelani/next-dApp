b0d030d94d110d5a3530329fa86ba6e4b6050dba
Matteo Celani <26189482+matteocelani@users.noreply.github.com>
2025-01-10 18:43:59 +0100

    Layout structure and styling improvements

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -5,46 +5,64 @@ import Link from 'next/link';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { NavigationProps } from '@/lib/types/layout';
 
+const navLinks = [
+  {
+    label: 'GitHub',
+    href: 'https://github.com/matteocelani/next-dApp',
+  },
+  {
+    label: 'Website',
+    href: 'https://www.mashu.dev/',
+  },
+];
+
 export default function NavBar({ isOpen, toggleOpen }: NavigationProps) {
   return (
     <header className="px-4 xs:px-6 md:px-8 bg-neutral-100 shadow-lg dark:bg-neutral-800">
       <nav className="flex items-center justify-between mx-auto">
-        <Link href="/" target="_self" className="py-4 pr-4">
-          <Image src="/img/logo.png" alt="logo" width={60} height={60} />
+        {/* Logo */}
+        <Link href="/" className="py-4 pr-4">
+          <Image
+            src="/img/logo.png"
+            alt="logo"
+            width={60}
+            height={60}
+            priority
+          />
         </Link>
 
+        {/* Desktop Navigation */}
         <div className="hidden md:flex items-center space-x-6">
-          <a
-            className="text-black text-lg font-medium dark:text-white"
-            href="https://github.com/matteocelani/next-dApp"
-            target="_blank"
-            rel="noreferrer"
-          >
-            GitHub
-          </a>
-          <a
-            className="text-black text-lg font-medium dark:text-white"
-            href="https://www.mashu.dev/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Website
-          </a>
+          {navLinks.map(({ label, href }) => (
+            <a
+              key={label}
+              href={href}
+              target="_blank"
+              rel="noreferrer"
+              className="text-black text-lg font-medium dark:text-white"
+            >
+              {label}
+            </a>
+          ))}
           <ConnectButton />
         </div>
 
+        {/* Mobile Navigation */}
         <div className="flex items-center md:hidden">
           <ConnectButton />
-          <div
-            onClick={toggleOpen}
-            className={`hamburger flex flex-col justify-between w-6 h-5 cursor-pointer ml-4 ${
-              isOpen ? 'open' : ''
-            }`}
+          <button
+            onClick={() => toggleOpen()}
+            className="ml-4"
+            aria-label="Toggle Menu"
           >
-            <span className="line"></span>
-            <span className="line"></span>
-            <span className="line"></span>
-          </div>
+            <div
+              className={`hamburger flex flex-col justify-between w-6 h-5 ${isOpen ? 'open' : ''}`}
+            >
+              <span className="line" />
+              <span className="line" />
+              <span className="line" />
+            </div>
+          </button>
         </div>
       </nav>
     </header>
